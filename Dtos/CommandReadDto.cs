0b836c55da5d77cf62cc9bc0d1dbe1a7bb79bc0e
DESKTOP-50U56HO <kamranvi@code.edu.az>
2023-01-05 00:03:28 +0400

    feat: Created Command DTOs

---
+++ Dtos/CommandReadDto.cs
@@ -0,0 +1,10 @@
+namespace MinApi.Dtos
+{
+    public class CommandReadDto
+    {
+        public int Id { get; set; }
+        public string? HowTo { get; set; }
+        public string? Platform { get; set; }
+        public string? CommandLine { get; set; }
+    }
+}
