105c0c2d0738952102f7e9d74626b2c4671245da
DESKTOP-50U56HO <kamranvi@code.edu.az>
2023-01-05 18:52:18 +0400

    feat: Built minimal API in Program class

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using AutoMapper;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using MinAPI.Data;
+using MinAPI.Dtos;
+using MinAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +22,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 });
 
 builder.Services.AddScoped<ICommandRepo, CommandRepo>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 
 var app = builder.Build();
@@ -30,5 +34,64 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("api/v1/commands", async (ICommandRepo repo, IMapper mapper) =>
+{
+    var commands = await repo.GetAllCommands();
+    return Results.Ok(mapper.Map<IEnumerable<CommandReadDto>>(commands));
+});
+
+app.MapGet("api/v1/commands/{id}", async (ICommandRepo repo, IMapper mapper, int id) =>
+{
+    var command = await repo.GetCommandById(id);
+    if (command != null)
+    {
+        return Results.Ok(mapper.Map<CommandReadDto>(command));
+    }
+
+    return Results.NotFound();
+});
+
+app.MapPost("api/v1/commands", async (ICommandRepo repo, IMapper mapper,
+    CommandCreateDto commandCreateDto) =>
+{
+    var commandModel = mapper.Map<Command>(commandCreateDto);
+
+    await repo.CreateCommand(commandModel);
+    await repo.SaveChanges();
+
+    var commandReadDto = mapper.Map<CommandReadDto>(commandModel);
+
+    return Results.Created($"api/v1/commands/{commandReadDto.Id}", commandReadDto);
+});
+
+app.MapPut("api/v1/commands/{id}", async (ICommandRepo repo, IMapper mapper, int id,
+    CommandUpdateDto commandUpdateDto) =>
+{
+    var command = await repo.GetCommandById(id);
+
+    if (command is null)
+        return Results.NotFound();
+
+    mapper.Map(commandUpdateDto, command);
+
+    await repo.SaveChanges();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("api/v1/commands/{id}", async (ICommandRepo repo, IMapper mapper, int id) =>
+{
+    var command = await repo.GetCommandById(id);
+
+    if (command is null)
+        return Results.NotFound();
+
+    repo.DeleteCommand(command);
+
+    await repo.SaveChanges();
+
+    return Results.NoContent();
+});
 
 app.Run();
